098b3654f423ecaff86f67fbb80e777c0765a2d6
Ralf Jung <post@ralfj.de>
2016-10-24 14:55:57 +0200

    WIP: uncurried sync spec

--- sync.v
+++ sync.v
@@ -7,12 +7,14 @@ From iris.heap_lang Require Import proofmode notation.
 Section sync.
   Context `{!heapG Σ} (N : namespace).
 
-  Definition synced R (f' f: val) :=
-    (□ ∀ P Q (x: val), ({{ R ★ P x }} f x {{ v, R ★ Q x v }}) →
-                       ({{ P x }} f' x {{ v, Q x v }}))%I.
-
+  (* TODO: Use our usual style with a generic post-condition. *)
+  (* TODO: We could get rid of the x, and hard-code a unit. That would
+     be no loss in expressiveness, but probably more annoying to apply.
+     How much more annoying? And how much to we gain in terms of things
+     becomign easier to prove? *)
   Definition is_syncer (R: iProp Σ) (s: val) :=
-    (□ ∀ (f : val), WP s f {{ f', synced R f' f }})%I.
+    (□ ∀ (f : val) P Q, (∀ x, {{ R ★ P x }} f x {{ v, R ★ Q x v }}) -★
+                    WP s f {{ f', ∀ x, {{ P x }} f' x {{ v, Q x v }} }})%I.
 
   Definition mk_syncer_spec (mk_syncer: val) :=
     ∀ (R: iProp Σ) (Φ: val -> iProp Σ),
